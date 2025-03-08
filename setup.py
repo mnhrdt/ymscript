ec40d9311da36539e355738a22c8f369989457f0
enric meinhardt-llopis <enric.meinhardt@ens-paris-saclay.fr>
2025-03-08 13:03:50 +0100

    add "joseph"-style resamplers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
 	name = "ymscript",
-	version = "11",
+	version = "12",
 	description = "Image Processing Scripts",
 	url="https://git.sr.ht/~coco/ymscript",
 	classifiers = [
